84c925527dc2097643fb8f0af8ce6e09210522d7
Sung Jae Hong <sroyhong313@gmail.com>
2017-04-11 11:05:08 -0400

    javadoc'd test files

--- app/src/main/java/com/gatech/edu/soloTechno/m4_login/controllers/NameValidator.java
+++ app/src/main/java/com/gatech/edu/soloTechno/m4_login/controllers/NameValidator.java
@@ -1,18 +1,18 @@
 package com.gatech.edu.soloTechno.m4_login.controllers;
 
-/**
- * Created by shong313 on 4/10/17.
- */
-
 public class NameValidator {
-
+    /**
+     * method that checks whether the name of the user is valid
+     * @param name the name of the user
+     * @return boolean that indicates whether the name is valid / able to be used
+     */
     public boolean isValidName(String name) {
 
         if (name == null) {
             return false;
         }
 
-        if(name.equals("")) {
+        if (name.equals("")) {
             return false;
         }
 
--- app/src/test/java/com/gatech/edu/soloTechno/m4_login/isValidNameTests.java
+++ app/src/test/java/com/gatech/edu/soloTechno/m4_login/isValidNameTests.java
@@ -2,50 +2,46 @@ package com.gatech.edu.soloTechno.m4_login;
 
 import com.gatech.edu.soloTechno.m4_login.controllers.NameValidator;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by shong313 on 4/10/17.
- */
-
 public class isValidNameTests {
         NameValidator checker;
 
-        @Before
-        public void setUp()
+    /**
+     * calls the nameValidator method to prepare setting up to check
+     */
+    @Before
+    public void setUp()
         {
             checker = new NameValidator();
         }
 
-        @After
-        public void tearDown()
-        {
+    /**
+     * Tests to check for the failing conditions
+     */
+    @Test
+    public void nameFailTest() {
 
-        }
-
-        @Test
-        public void nameFailTest() {
-
-            System.out.println("Testing Failing Conditions:isValidName");
+        System.out.println("Testing Failing Conditions:isValidName");
 
 
-            // test the failing conditions
-            assertEquals(false, checker.isValidName(""));
-            assertEquals(false, checker.isValidName(null));
-        }
+        // test the failing conditions
+        assertEquals(false, checker.isValidName(""));
+        assertEquals(false, checker.isValidName(null));
+    }
 
-        @Test
-        public void nameSuccessTest() {
+    /**
+     * Tests to check for the success conditions
+     */
+    @Test
+    public void nameSuccessTest() {
+        System.out.println("Testing Success Conditions: isValidName");
 
-            System.out.println("Testing Success Conditions: isValidName");
-
-            // test the passing conditions
-            assertEquals(true, checker.isValidName("Roy"));
-            assertEquals(true, checker.isValidName("Joshua"));
-        }
-
+        // test the passing conditions
+        assertEquals(true, checker.isValidName("Roy"));
+        assertEquals(true, checker.isValidName("Joshua"));
+    }
 }
